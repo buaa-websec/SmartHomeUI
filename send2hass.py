09b582d76948ce2c514a4deffb5dbcf3e1eb4f73
buaa-websec <57474179+buaa-websec@users.noreply.github.com>
2020-01-17 17:49:23 +0800

    正式版
    所有基本功能完成

--- send2hass.py
+++ send2hass.py
@@ -11,7 +11,7 @@ def change_state(entity_id, new_state):
     }
     
     headers = {
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIyOWUzYjcwZGZkMTE0NzQ2YjFkOTU2YmJiODQ4NzNmOSIsImlhdCI6MTU3Mjk1OTc1MCwiZXhwIjoxODg4MzE5NzUwfQ.dG8UmC0FlAdv4xCEf3LdK9OGvDx3AIcnOzVbVre_b2w',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYTI0MzBkMmEyN2E0OTc1ODUyZmQ5NzQwNDA4YWQ0NSIsImlhdCI6MTU3OTA4NTMzMywiZXhwIjoxODk0NDQ1MzMzfQ.jrMlPWQEdjfGfHMvzKk0DTf2fx4CzMkYsmewk5bw5tk',
         'content-type': 'application/json',
     }
    
@@ -20,19 +20,3 @@ def change_state(entity_id, new_state):
 
 
   
-#    change_state('light.l008', 'on')
-#   self.pushButton_90.clicked.connect(lambda:lightsclick(self.pushButton_90,'l008'))
-
-#lights = {'l008':1, 'l001':1, 'l004':1,'l011':1}
-
-# def lightsclick(button,lname):
-#     entity_id = 'light.'+lname
-#     if lights[lname] == 1:
-#         button.setStyleSheet("background-color: rgb(255, 255, 255);")
-#         new_state='off'    
-#         lights[lname] = 0
-#     else:
-#         button.setStyleSheet("background-color: rgb(0, 255, 0);")
-#         new_state='on'    
-#         lights[lname] = 1
-#     change_state(entity_id, new_state)
